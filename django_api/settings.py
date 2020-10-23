1771835624d1ef36c91a61d1c3dcc455869df350
Ben Garlock <ben.garlock.gmail.com>
2020-10-23 12:43:41 -0400

    updated settings.py to include bengarlock.com in allowed hosts

--- django_api/settings.py
+++ django_api/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'kvii(44mua(enoyd4_dj66qqzfo$11=(np4jp^598gdbkzs1v#'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.86.51', '127.0.0.1', 'localhost', 'yoda']
+ALLOWED_HOSTS = ['192.168.86.51', '127.0.0.1', 'localhost', 'yoda', 'bengarlock.com']
 
 
 # Application definition
