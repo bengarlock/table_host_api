dd45b348aaac57f6f6c95278064f697b7a61f886
Ben Garlock <evilbunnyrabbits@gmail.com>
2020-10-27 18:10:42 -0400

    seed file almost done

--- slots/serializers.py
+++ slots/serializers.py
@@ -5,4 +5,3 @@ class SlotSerializer(serializers.ModelSerializer):
     class Meta:
         model = Slot
         fields = ("id", 'booked', 'time', 'party_size', 'status', 'tables', 'book_id')
-        depth = 1
