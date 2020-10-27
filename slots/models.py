6ec2c2b3e4fa7391298b78d10f5903577633f5b9
Ben Garlock <evilbunnyrabbits@gmail.com>
2020-10-27 17:49:27 -0400

    updated models to default 0 on book id

--- slots/models.py
+++ slots/models.py
@@ -7,8 +7,8 @@ class Slot(models.Model):
     time = models.TextField(default="5:00 PM")
     party_size = models.IntegerField(default=0)
     status = models.TextField(blank=True)
-    tables = ArrayField(models.CharField(max_length=15), default=[], blank=True)
-    book_id = models.ForeignKey(Book, related_name="slots", on_delete=models.SET_NULL, required=False)
+    tables = ArrayField(models.CharField(max_length=15), default=list, blank=True)
+    book_id = models.ForeignKey(Book, related_name="slots", on_delete=models.SET_DEFAULT, default=5)
 
 
 
