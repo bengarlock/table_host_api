12fc5638c95e4bbc71cbac47277c60cbf83935e8
Ben Garlock <evilbunnyrabbits@gmail.com>
2020-10-27 19:55:33 -0400

    almost there...

--- slots/models.py
+++ slots/models.py
@@ -8,6 +8,7 @@ class Slot(models.Model):
     time = models.TextField(default="5:00 PM")
     party_size = models.IntegerField(default=0)
     status = models.TextField(blank=True)
+    reservation_notes = models.TextField(blank=True)
     tables = ArrayField(models.CharField(max_length=15), default=list, blank=True)
     book = models.ForeignKey(Book, related_name="slots", on_delete=models.SET_DEFAULT, default=1)
     guest = models.ForeignKey(Guest, related_name="guest", on_delete=models.SET_DEFAULT, default=1)
