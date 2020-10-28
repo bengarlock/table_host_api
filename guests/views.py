c5a7a9d89b300bdbfcbcb788309ff656650d2ad4
Ben Garlock <evilbunnyrabbits@gmail.com>
2020-10-28 11:57:48 -0400

    testing guest search

--- guests/views.py
+++ guests/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Guest
 from .serializers import GuestSerializer
+import django_filters.rest_framework
 
 class GuestView(viewsets.ModelViewSet):
     queryset = Guest.objects.all()
     serializer_class = GuestSerializer
+    filterset_fields = ['first_name', 'last_name']
