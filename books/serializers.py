163d6657ab3bae20d1170869f19571837fcaa149
Ben Garlock <evilbunnyrabbits@gmail.com>
2020-10-30 13:34:44 -0400

    got slot to update guest id, but cant nested guest data to update correctly

--- books/serializers.py
+++ books/serializers.py
@@ -1,6 +1,20 @@
 from rest_framework import serializers
 from .models import Book
-from slots.serializers import SlotSerializer
+from guests.models import Guest
+from slots.models import Slot
+
+
+class GuestSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Guest
+        fields = '__all__'
+
+
+class SlotSerializer(serializers.ModelSerializer):
+    guest = GuestSerializer()
+    class Meta:
+        model = Slot
+        fields = ['id', 'booked', 'time', 'party_size', 'status', 'reservation_notes', 'tables', 'book', 'guest']
 
 
 class BookSerializer(serializers.ModelSerializer):
@@ -9,4 +23,4 @@ class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         fields = '__all__'
-        depth = 1
+        depth = 2
--- slots/serializers.py
+++ slots/serializers.py
@@ -1,15 +1,10 @@
 from rest_framework import serializers, fields
 from .models import Slot
 from .models import Guest
-
-class GuestSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Guest
-        fields = ['id', 'first_name', 'last_name', 'phone_number', 'guest_notes', 'root_user']
+from guests.serializers import GuestSerializer
 
 
 class SlotSerializer(serializers.ModelSerializer):
-    guest = serializers.SerializerMethodField('get_guest_data')
 
     class Meta:
         model = Slot
@@ -25,7 +20,6 @@ class SlotSerializer(serializers.ModelSerializer):
             "guest_notes": slot.guest.guest_notes,
             "root_user": slot.guest.root_user,
         }
-
         return guest
 
     def create(self, validated_data):
@@ -33,25 +27,13 @@ class SlotSerializer(serializers.ModelSerializer):
         return Slot(**validated_data)
 
     def update(self, instance, validated_data):
-        guest_data = validated_data.pop('guest')
-        print(validated_data)
-
         instance.booked = validated_data.get('booked', instance.booked)
         instance.time = validated_data.get('time', instance.time)
         instance.party_size = validated_data.get('party_size', instance.party_size)
         instance.status = validated_data.get('status', instance.status)
         instance.reservation_notes = validated_data.get('reservation_notes', instance.reservation_notes)
         instance.tables = validated_data.get('tables', instance.tables)
+        instance.guest = validated_data.get('guest', instance.guest)
         instance.save()
-
-
-        #
-        # for item in guest_data:
-        #      item.first_name = guest_data.get('first_name', item.first_name)
-        #      item.last_name = guest_data.get('last_name', item.last_name)
-        #      item.phone_number = guest_data.get('phone_number', item.phone_number)
-        #      item.guest_notes = guest_data.get('guest_notes', item.guest_notes)
-        #      item.root_user = guest_data.get('root_user', item.root_user)
-        #      item.save()
 
         return instance
