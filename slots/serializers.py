a9536e59d30bbcb2db44646cfcf4a67b46be7ab3
Ben Garlock <evilbunnyrabbits@gmail.com>
2020-10-30 10:43:39 -0400

    updated slot serializer

--- slots/serializers.py
+++ slots/serializers.py
@@ -4,6 +4,8 @@ from guests.serializers import GuestSerializer
 
 class SlotSerializer(serializers.ModelSerializer):
 
+    guest = GuestSerializer()
+
     class Meta:
         model = Slot
         fields = '__all__'
@@ -12,8 +14,18 @@ class SlotSerializer(serializers.ModelSerializer):
         return Slot(**validated_data)
 
     def update(self, instance, validated_data):
-        print(instance)
+
         instance.booked = validated_data.get('booked', instance.booked)
+        instance.time = validated_data.get('time', instance.time)
+        instance.party_size = validated_data.get('party_size', instance.party_size)
+        instance.status = validated_data.get('status', instance.status)
+        instance.reservation_notes = validated_data.get('reservation_notes', instance.reservation_notes)
+        instance.tables = validated_data.get('tables', instance.tables)
+        guest = validated_data.get('guest')
+
+        for item in guest:
+            pass
+
         return instance
 
 
