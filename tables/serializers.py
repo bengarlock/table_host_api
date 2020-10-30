9e3c384bd870e7236ecf7f5c84e82389a07fc77d
Ben Garlock <evilbunnyrabbits@gmail.com>
2020-10-30 15:22:21 -0400

    added update method to table serializer

--- tables/serializers.py
+++ tables/serializers.py
@@ -6,3 +6,12 @@ class TableSerializer(serializers.ModelSerializer):
     class Meta:
         model = Table
         fields = '__all__'
+
+        def update(self, instance, validated_data):
+            instance.class_name = validated_data.get('class_name', instance.class_name)
+            instance.position_left = validated_data.get('position_left', instance.position_left)
+            instance.position_top = validated_data.get('position_top', instance.position_top)
+            instance.name = validated_data.get('name', instance.name)
+            instance.restaurant_id = validated_data.get('restaurant_id', instance.restaurant_id)
+            instance.status = validated_data.get('status', instance.status)
+            instance.save()
