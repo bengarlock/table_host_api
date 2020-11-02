de0debbeb2d66e6c28948197771d69f64242775b
Ben Garlock <evilbunnyrabbits@gmail.com>
2020-11-02 13:19:50 -0500

    added phone number cleanup logic

--- guests/serializers.py
+++ guests/serializers.py
@@ -1,8 +1,31 @@
 from rest_framework import serializers
 from .models import Guest
 
-class GuestSerializer(serializers.ModelSerializer):
 
+class GuestSerializer(serializers.ModelSerializer):
     class Meta:
         model = Guest
         fields = '__all__'
+
+    def update(self, instance, validated_data):
+
+        phone_number = instance.phone_number
+        str(phone_number)
+        bad_chars = [';', ':', '!', "*", ",", ".", "-", "+1"]
+
+        for i in bad_chars:
+            phone_number = phone_number.replace(i, '')
+
+        validated_data["phone_number"] = phone_number
+
+
+        if instance.root_user:
+            pass
+        else:
+            instance.first_name = validated_data.get('first_name', instance.first_name)
+            instance.last_name = validated_data.get('last_name', instance.last_name)
+            instance.phone_number = validated_data.get('phone_number', instance.phone_number)
+            instance.guest_notes = validated_data.get('guest_notes', instance.guest_notes)
+            instance.save()
+
+        return instance
--- seed.py
+++ seed.py
@@ -4,7 +4,7 @@ import json
 from faker import Faker
 import random
 
-#url = "http://127.0.0.1:8000/"
+# url = "http://127.0.0.1:8000/"
 url = "http://www.bengarlock.com:8080/"
 
 today = date.today()
@@ -32,7 +32,7 @@ def create_books(date_cap):
             "restaurant_id": 1,
             "slots": [],
         }
-        book = requests.post(url = url + "books/", data=obj)
+        book = requests.post(url=url + "books/", data=obj)
         r = book.content.decode('UTF-8')
         res = json.loads(r)
         books.append(res["id"])
@@ -83,7 +83,7 @@ def create_guests(data_cap):
         last_name = name[1]
         phone_number = fake.phone_number()
 
-        notes = "Works for " + str(fake.company()) + " " + notes_array[random.randint(0, len(notes_array)-1)]
+        notes = "Works for " + str(fake.company()) + " " + notes_array[random.randint(0, len(notes_array) - 1)]
 
         obj = {
             "first_name": first_name,
@@ -97,46 +97,72 @@ def create_guests(data_cap):
         # print(guest.content)
         index += 1
 
-def create_tables():
 
+def create_tables():
     tables = [
 
-        {"class_name": "two-top-horizontal", "position_left": "102px", "position_top": "40px", "name": "1", "restaurant_id": 1, "status": "done"},
-        {"class_name": "two-top-horizontal", "position_left": "168px", "position_top": "40px", "name": "2", "restaurant_id": 1, "status": "done"},
-        {"class_name": "two-top-horizontal", "position_left": "234px", "position_top": "40px", "name": "3", "restaurant_id": 1, "status": "done"},
-        {"class_name": "two-top-horizontal", "position_left": "300px", "position_top": "40px", "name": "4", "restaurant_id": 1, "status": "done"},
-        {"class_name": "two-top-horizontal", "position_left": "364px", "position_top": "40px", "name": "5", "restaurant_id": 1, "status": "done"},
-        {"class_name": "two-top-horizontal", "position_left": "430px", "position_top": "40px", "name": "6", "restaurant_id": 1, "status": "done"},
-        {"class_name": "two-top-horizontal", "position_left": "40px", "position_top": "120px", "name": "7", "restaurant_id": 1, "status": "done"},
-        {"class_name": "two-top-horizontal", "position_left": "40px", "position_top": "180px", "name": "8", "restaurant_id": 1, "status": "done"},
-        {"class_name": "two-top-horizontal", "position_left": "40px", "position_top": "240px", "name": "9", "restaurant_id": 1, "status": "done"},
-        {"class_name": "two-top-horizontal", "position_left": "40px", "position_top": "300px", "name": "10", "restaurant_id": 1, "status": "done"},
-        {"class_name": "two-top-horizontal", "position_left": "40px", "position_top": "360px", "name": "11", "restaurant_id": 1, "status": "done"},
-        {"class_name": "two-top-horizontal", "position_left": "40px", "position_top": "420px", "name": "12", "restaurant_id": 1, "status": "done"},
-        {"class_name": "fourTop", "position_left": "140px", "position_top": "150px", "name": "14", "restaurant_id": 1, "status": "done"},
-        {"class_name": "fourTop", "position_left": "210px", "position_top": "150px", "name": "15", "restaurant_id": 1, "status": "done"},
-        {"class_name": "fourTop", "position_left": "280px", "position_top": "150px", "name": "16", "restaurant_id": 1, "status": "done"},
-        {"class_name": "fourTop", "position_left": "350px", "position_top": "150px", "name": "17", "restaurant_id": 1, "status": "done"},
-        {"class_name": "fourTop", "position_left": "420px", "position_top": "150px", "name": "18", "restaurant_id": 1, "status": "done"},
-        {"class_name": "fourTop", "position_left": "140px", "position_top": "250px", "name": "19", "restaurant_id": 1, "status": "done"},
-        {"class_name": "fourTop", "position_left": "210px", "position_top": "250px", "name": "20", "restaurant_id": 1, "status": "done"},
-        {"class_name": "fourTop", "position_left": "280px", "position_top": "250px", "name": "21", "restaurant_id": 1, "status": "done"},
-        {"class_name": "fourTop", "position_left": "350px", "position_top": "250px", "name": "22", "restaurant_id": 1, "status": "done"},
-        {"class_name": "fourTop", "position_left": "420px", "position_top": "250px", "name": "23", "restaurant_id": 1, "status": "done"},
-        {"class_name": "fourTop", "position_left": "140px", "position_top": "350px", "name": "24", "restaurant_id": 1, "status": "done"},
-        {"class_name": "fourTop", "position_left": "210px", "position_top": "350px", "name": "25", "restaurant_id": 1, "status": "done"},
-        {"class_name": "fourTop", "position_left": "280px", "position_top": "350px", "name": "26", "restaurant_id": 1, "status": "done"},
-        {"class_name": "fourTop", "position_left": "350px", "position_top": "350px", "name": "27", "restaurant_id": 1, "status": "done"},
-        {"class_name": "fourTop", "position_left": "420px", "position_top": "350px", "name": "28", "restaurant_id": 1, "status": "done"},
+        {"class_name": "two-top-horizontal", "position_left": "102px", "position_top": "40px", "name": "1",
+         "restaurant_id": 1, "status": "done"},
+        {"class_name": "two-top-horizontal", "position_left": "168px", "position_top": "40px", "name": "2",
+         "restaurant_id": 1, "status": "done"},
+        {"class_name": "two-top-horizontal", "position_left": "234px", "position_top": "40px", "name": "3",
+         "restaurant_id": 1, "status": "done"},
+        {"class_name": "two-top-horizontal", "position_left": "300px", "position_top": "40px", "name": "4",
+         "restaurant_id": 1, "status": "done"},
+        {"class_name": "two-top-horizontal", "position_left": "364px", "position_top": "40px", "name": "5",
+         "restaurant_id": 1, "status": "done"},
+        {"class_name": "two-top-horizontal", "position_left": "430px", "position_top": "40px", "name": "6",
+         "restaurant_id": 1, "status": "done"},
+        {"class_name": "two-top-horizontal", "position_left": "40px", "position_top": "120px", "name": "7",
+         "restaurant_id": 1, "status": "done"},
+        {"class_name": "two-top-horizontal", "position_left": "40px", "position_top": "180px", "name": "8",
+         "restaurant_id": 1, "status": "done"},
+        {"class_name": "two-top-horizontal", "position_left": "40px", "position_top": "240px", "name": "9",
+         "restaurant_id": 1, "status": "done"},
+        {"class_name": "two-top-horizontal", "position_left": "40px", "position_top": "300px", "name": "10",
+         "restaurant_id": 1, "status": "done"},
+        {"class_name": "two-top-horizontal", "position_left": "40px", "position_top": "360px", "name": "11",
+         "restaurant_id": 1, "status": "done"},
+        {"class_name": "two-top-horizontal", "position_left": "40px", "position_top": "420px", "name": "12",
+         "restaurant_id": 1, "status": "done"},
+        {"class_name": "fourTop", "position_left": "140px", "position_top": "150px", "name": "14", "restaurant_id": 1,
+         "status": "done"},
+        {"class_name": "fourTop", "position_left": "210px", "position_top": "150px", "name": "15", "restaurant_id": 1,
+         "status": "done"},
+        {"class_name": "fourTop", "position_left": "280px", "position_top": "150px", "name": "16", "restaurant_id": 1,
+         "status": "done"},
+        {"class_name": "fourTop", "position_left": "350px", "position_top": "150px", "name": "17", "restaurant_id": 1,
+         "status": "done"},
+        {"class_name": "fourTop", "position_left": "420px", "position_top": "150px", "name": "18", "restaurant_id": 1,
+         "status": "done"},
+        {"class_name": "fourTop", "position_left": "140px", "position_top": "250px", "name": "19", "restaurant_id": 1,
+         "status": "done"},
+        {"class_name": "fourTop", "position_left": "210px", "position_top": "250px", "name": "20", "restaurant_id": 1,
+         "status": "done"},
+        {"class_name": "fourTop", "position_left": "280px", "position_top": "250px", "name": "21", "restaurant_id": 1,
+         "status": "done"},
+        {"class_name": "fourTop", "position_left": "350px", "position_top": "250px", "name": "22", "restaurant_id": 1,
+         "status": "done"},
+        {"class_name": "fourTop", "position_left": "420px", "position_top": "250px", "name": "23", "restaurant_id": 1,
+         "status": "done"},
+        {"class_name": "fourTop", "position_left": "140px", "position_top": "350px", "name": "24", "restaurant_id": 1,
+         "status": "done"},
+        {"class_name": "fourTop", "position_left": "210px", "position_top": "350px", "name": "25", "restaurant_id": 1,
+         "status": "done"},
+        {"class_name": "fourTop", "position_left": "280px", "position_top": "350px", "name": "26", "restaurant_id": 1,
+         "status": "done"},
+        {"class_name": "fourTop", "position_left": "350px", "position_top": "350px", "name": "27", "restaurant_id": 1,
+         "status": "done"},
+        {"class_name": "fourTop", "position_left": "420px", "position_top": "350px", "name": "28", "restaurant_id": 1,
+         "status": "done"},
     ]
-
     for table in tables:
         table = requests.post(url + "tables/", data=table)
         print(table.content)
 
 
-# create_root_user()
-# create_books(10)
-# create_slots(array=books)
-# create_guests(100)
+create_root_user()
+create_books(10)
+create_slots(array=books)
+create_guests(100)
 create_tables()
