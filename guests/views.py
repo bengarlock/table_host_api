eb48d7bd23e3a93eeb8b65e2817a8f77186dd1da
Ben Garlock <evilbunnyrabbits@gmail.com>
2020-11-02 13:39:18 -0500

    got general search working... or close to

--- guests/views.py
+++ guests/views.py
@@ -1,11 +1,17 @@
+from typing import List
+
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
 from .models import Guest
 from .serializers import GuestSerializer
-import django_filters.rest_framework
+
 
 class GuestView(viewsets.ModelViewSet):
     queryset = Guest.objects.all()
     serializer_class = GuestSerializer
 
-    filterset_fields = ["first_name"]
+    filter_backends = [filters.SearchFilter]
+
+    search_fields = ["first_name", "last_name", "phone_number"]
+
+#http://127.0.0.1:8000/guests/?search=mcdaniel
