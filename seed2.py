6e94b79fd149c470f2a8bac900a778fa5bb3a20f
Ben Garlock <evilbunnyrabbits@gmail.com>
2020-11-09 13:17:03 -0500

    added restaurant model

--- seed2.py
+++ seed2.py
@@ -1,11 +1,12 @@
 from guests.models import Guest
+from restaurants.models import Restaurant
 
-root_user = Guest.objects.create(
-    first_name='',
-    last_name='',
-    phone_number='',
-    guest_notes='',
-    root_user=True,
-    slot=[]
-)
+# root_user = Guest.objects.create(
+#     first_name='',
+#     last_name='',
+#     phone_number='',
+#     guest_notes='',
+#     root_user=True,
+#     slot=[]
+# )
 
