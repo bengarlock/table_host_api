9ae9a875bd9bb47793e53949a21eb670eaf44a41
Ben Garlock <evilbunnyrabbits@gmail.com>
2020-11-09 13:51:38 -0500

    added restaurant view to urls and created restaurant view.py

--- django_api/urls.py
+++ django_api/urls.py
@@ -3,6 +3,7 @@ from books.views import BookView
 from guests.views import GuestView
 from slots.views import SlotView
 from tables.views import TableView
+from restaurants.views import RestaurantView
 from django.contrib import admin
 from django.conf.urls import url
 
@@ -12,6 +13,7 @@ router.register(r'books', BookView)
 router.register(r'guests', GuestView)
 router.register(r'slots', SlotView)
 router.register(r'tables', TableView)
+router.register(r'restaurants', RestaurantView)
 
 urlpatterns = [
     url('admin/', admin.site.urls),
--- restaurants/views.py
+++ restaurants/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import viewsets, filters
+from .models import Restaurant
+
+
+class RestaurantView(viewsets.ModelViewSet):
+    queryset = Restaurant.objects.all()
