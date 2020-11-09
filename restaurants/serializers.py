f8f0e8d04b86aa8d51d6ac15894ea89d5924ee6b
Ben Garlock <evilbunnyrabbits@gmail.com>
2020-11-09 13:54:57 -0500

    added restaurant view to urls and created restaurant view.py

---
+++ restaurants/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from .models import Restaurant
+
+
+class RestaurantSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Restaurant
+        fields = ['name']
--- restaurants/views.py
+++ restaurants/views.py
@@ -3,7 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 from rest_framework import viewsets, filters
 from .models import Restaurant
+from .serializers import RestaurantSerializer
 
 
 class RestaurantView(viewsets.ModelViewSet):
     queryset = Restaurant.objects.all()
+    serializer_class = RestaurantSerializer
